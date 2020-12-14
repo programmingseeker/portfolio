ce7e5d165ad06f0a3340b660059e22aa05939b95
Gaurav-S-Rao <gauravsrao.dev@gmail.com>
2020-12-14 18:49:46 +0530

    responsive contact page;

--- src/components/profileCard.js
+++ src/components/profileCard.js
@@ -1,48 +1,162 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-function profileCard() {
+function ProfileCard() {
+  // const [active, setActive] = useState(false)
+  const Image = ({ customer }) => {
+    const data = useStaticQuery(graphql`
+      query {
+        customer1: file(relativePath: { eq: "customer-profile1.jpg" }) {
+          childImageSharp {
+            fixed(width: 100, height: 100) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+        customer2: file(relativePath: { eq: "customer-profile2.jpg" }) {
+          childImageSharp {
+            fixed(width: 100, height: 100) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `)
+
+    function errorHandler() {
+      return <div>Picture not found</div>
+    }
+
+    if (!data?.customer1?.childImageSharp?.fixed) {
+    } else if (!data?.customer2?.childImageSharp?.fixed) {
+      return <div>Picture not found</div>
+    }
+    switch (customer) {
+      case 1:
+        return (
+          <Img
+            fixed={data.customer1.childImageSharp.fixed}
+            onError={errorHandler}
+          />
+        )
+      case 2:
+        return <Img fixed={data.customer2.childImageSharp.fixed} />
+      default:
+        return ""
+    }
+  }
+
+  // <div className="container profile-card-container">
+  //   <div className="profile-card">
+  //     <div className="profile-photo">
+  //       <Image customer={1} />
+  //     </div>
+  //     <div className="profile-desc">
+  //       <h3>
+  //         Gaurav S Rao
+  //         <br /> <span>Full Stack Developer</span>
+  //       </h3>
+  //       <p className="text-wrap">akljfshgdkjahsgfkjhsfdkjhsdfkgjkhjsfdg</p>
+  //     </div>
+  //   </div>
+  //   <div className="profile-card">
+  //     <div className="profile-photo">
+  //       <Image customer={2} />
+  //     </div>
+  //     <div className="profile-desc">
+  //       <h3>
+  //         Krtin U Shet
+  //         <br /> <span>Full Stack Developer</span>
+  //       </h3>
+  //       <p className="text-wrap">akljfshgdkjahsgfkjhsfdkjhsdfkgjkhjsfdg</p>
+  //     </div>
+  //   </div>
+  // </div>
   return (
-    <div className="profile-card">
-      <div id="profile">
-        <img
-          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/225748/profile.jpg"
-          alt="User"
-        />
-        <h1>Iva Dopuđ</h1>
-        <p>Front-end Web Developer</p>
-        <div class="info">
-          <i class="fa fa-info fa-1x block"></i>
-          <i class="fa fa-angle-down fa-2x block"></i>
-          <p>
-            In short, I spend my earthly time striving to create some darned
-            awesome UI designs, and also develop a few. Student @{" "}
-            <a href="http://en.ict.edu.rs/" target="_blank">
-              ICT College
+    <>
+      <div className="modal">
+        <div className="profile-photo">
+          <Image customer={1} />
+        </div>
+        <div className="close"></div>
+      </div>
+
+      <div className="container profile-card-container">
+        <div className="profile-card">
+          <div className="card-header">
+            <div className="hamburger-menu">
+              <div className="center"></div>
+            </div>
+            <a href="!#" className="mail">
+              <i className="far fa-envelope"></i>
             </a>
-            .
-          </p>
-          <p>Find me on:</p>
-          <a
-            class="link"
-            href="https://twitter.com/Masquetina/"
-            target="_blank"
-          >
-            <i class="fa fa-twitter fa-2x social"></i>
-          </a>
-          <a
-            class="link"
-            href="http://dribbble.com/Masquetina/"
-            target="_blank"
-          >
-            <i class="fa fa-dribbble fa-2x social"></i>
-          </a>
-          <a class="link" href="https://codepen.io/Masquetina" target="_blank">
-            <i class="fa fa-codepen fa-2x social"></i>
-          </a>
+            <div className="main">
+              <div className="image">
+                <div className="hover">
+                  <i className="fas fa-camera fa-2x"></i>
+                </div>
+              </div>
+              <h3 className="name">John Doe</h3>
+              <h3 className="sub-name">@J_Doe</h3>
+            </div>
+          </div>
+
+          <div className="content">
+            <div className="left">
+              <div className="profile-about-container">
+                <h3 className="title">About</h3>
+                <p className="text">
+                  Lorem Ipsum is simply text of the printing and types industry.
+                </p>
+              </div>
+              <div className="icons-container">
+                <a href="!#" className="icon">
+                  <i className="fab fa-facebook"></i>
+                </a>
+                <a href="!#" className="icon">
+                  <i className="fab fa-instagram"></i>
+                </a>
+                <a href="!#" className="icon">
+                  <i className="fab fa-google-plus-g"></i>
+                </a>
+                <a href="!#" className="icon">
+                  <i className="fab fa-twitter"></i>
+                </a>
+              </div>
+              <div className="buttons-wrap">
+                <div className="follow-wrap">
+                  <a href="!#" className="follow">
+                    Follow
+                  </a>
+                </div>
+                <div className="share-wrap">
+                  <a href="!#" className="share">
+                    Share
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="right">
+              <div>
+                <h3 className="number">91</h3>
+                <h3 className="number-title">Posts</h3>
+              </div>
+              <div>
+                <h3 className="number">2.4K</h3>
+                <h3 className="number-title">Following</h3>
+              </div>
+              <div>
+                <h3 className="number">4.7K</h3>
+                <h3 className="number-title">Followers</h3>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
-export default profileCard
+export default ProfileCard
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,180 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout"
-function contact() {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faMapMarkedAlt,
+  faEnvelope,
+  faPhoneAlt,
+} from "@fortawesome/free-solid-svg-icons"
+import {
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons"
+function Contact() {
+  const [focus, setFocus] = useState(null)
+  const [username, setusername] = useState("")
+  const [email, setemail] = useState("")
+  const [phone, setphone] = useState(null)
+  const [message, setmessage] = useState("")
+  const [alert, setalert] = useState(false)
   return (
     <>
       <Layout>
-        <h1>contact page</h1>
+        <div className="container-contact">
+          <span className="bg-circle"></span>
+          <div className="form-contact">
+            <div className="contact-info ">
+              <h3 className="title-contact">Contact Information</h3>
+              <p className="text-contact">
+                <span className="desc-small">
+                  Fill up the form and out Team
+                </span>{" "}
+                Will get back to you within 24 hours
+              </p>
+              <div className="info">
+                <div className="information d-flex align-items-center ">
+                  <FontAwesomeIcon
+                    icon={faMapMarkedAlt}
+                    className="mr-2 info-icon"
+                    style={{ fontSize: "25px" }}
+                  ></FontAwesomeIcon>
+                  <p> Bangalore India 560079</p>
+                </div>
+                <div className="information d-flex align-items-center ">
+                  <FontAwesomeIcon
+                    icon={faEnvelope}
+                    className=" mr-2 info-icon"
+                    style={{ fontSize: "25px" }}
+                  ></FontAwesomeIcon>
+                  <p>programmingseeker@gmail.com</p>
+                </div>
+                <div className="information d-flex align-items-center ">
+                  <FontAwesomeIcon
+                    icon={faPhoneAlt}
+                    className="mr-2 info-icon"
+                    style={{ fontSize: "25px" }}
+                  ></FontAwesomeIcon>
+                  <p>+91 9731362928/</p>
+                </div>
+              </div>
+              <div className="social-media">
+                <div className="social-icons">
+                  <a href="!#">
+                    <FontAwesomeIcon icon={faFacebookF}></FontAwesomeIcon>
+                  </a>
+                  <a href="!#">
+                    <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+                  </a>
+                  <a href="!#">
+                    <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+                  </a>
+                  <a href="!#">
+                    <FontAwesomeIcon
+                      icon={faLinkedinIn}
+                      className="fab fa-linkedin-in"
+                    ></FontAwesomeIcon>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="contact-form">
+              <span className="circle one"></span>
+              <span className="circle two"></span>
+
+              <form className="form-contact-text" autocomplete="off">
+                <h3 className="title-contact">Contact Developers</h3>
+                <div className={`input-container ${focus ? "focus" : null}`}>
+                  <input
+                    onFocus={() => {
+                      setFocus("focus")
+                    }}
+                    onBlur={() => {
+                      setFocus(null)
+                    }}
+                    type="text-contact"
+                    name="name"
+                    className="input"
+                    value={username}
+                    onChange={e => {
+                      setusername(e.target.value)
+                    }}
+                  />
+                  <label>Username</label>
+                  <span>Username</span>
+                </div>
+                <div className={`input-container ${focus ? "focus" : null}`}>
+                  <input
+                    onFocus={() => {
+                      setFocus("focus")
+                    }}
+                    onBlur={() => {
+                      setFocus(null)
+                    }}
+                    type="email"
+                    name="email"
+                    className="input"
+                    value={email}
+                    onChange={e => {
+                      setemail(e.target.value)
+                    }}
+                  />
+                  <label>Email</label>
+                  <span>Email</span>
+                </div>
+                <div className={`input-container ${focus ? "focus" : null}`}>
+                  <input
+                    onFocus={() => {
+                      setFocus("focus")
+                    }}
+                    onBlur={() => {
+                      setFocus(null)
+                    }}
+                    type="numeric"
+                    name="phone"
+                    className="input"
+                    value={phone}
+                    onChange={e => {
+                      setphone(e.target.value)
+                    }}
+                  />
+                  <label>Phone</label>
+                  <span>Phone</span>
+                </div>
+                <div
+                  className={`input-container textareaout ${
+                    focus ? "focus" : null
+                  }`}
+                >
+                  <textarea
+                    name="message"
+                    className="input textarea"
+                    value={message}
+                    onFocus={() => {
+                      setFocus("focus")
+                    }}
+                    onBlur={() => {
+                      setFocus(null)
+                    }}
+                    onChange={e => {
+                      setmessage(e.target.value)
+                    }}
+                  ></textarea>
+                  <label>Message</label>
+                  <span>Message</span>
+                </div>
+                <button type="submit" className="sumbit-contact-btn">
+                  Send
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
       </Layout>
     </>
   )
 }
 
-export default contact
+export default Contact
